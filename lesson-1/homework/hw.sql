d7bd5675fcc464f503a629b481504b915ecc1c8f
Asilbek-A <samsung9945008@gmail.com>
2025-07-02 19:10:31 +0300

    Update hw.sql

--- lesson-1/homework/hw.sql
+++ lesson-1/homework/hw.sql
@@ -1,2 +1,12 @@
-Asilbek
-sf
+create database SchoolDB 
+	create table Students (StudentID INT, Ism VARCHAR(50), Age INT)
+
+	insert into Students values (413, 'Asilbek', 23),(521, 'Aziz', 26)
+
+select * from Students
+
+| Tushuncha      | Turi           | Vazifasi                                       |
+| -------------- | -------------- | ---------------------------------------------- |
+| **SQL Server** | Dastur (DBMS)  | Ma'lumotlar bazalarini boshqaradi, saqlaydi    |
+| **SSMS**       | Dastur (GUI)   | SQL Server bilan qulay ishlash uchun interfeys |
+| **SQL**        | Til (language) | So‘rovlar yozish va DBMS bilan muloqot qilish  |
